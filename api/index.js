ae03ee2a8b2bce77ff63504c7800bf1d4e8ebca7
smallTwig <he.yuha@northeastern.edu>
2024-01-23 16:42:24 -0800

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.route.js'
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -11,9 +12,11 @@ mongoose.connect(process.env.MONGO).then(() => {
     });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3001, () => {
     console.log('server is running on port 3001');
 });
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
